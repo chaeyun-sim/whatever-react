af86c02e17ccf24c50728096fd8ff2e62c5586e9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-22 15:12:04 +0900

    [Feat] 2개 이상의 컴포넌트 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Header from './components/Header';
+import Header from '@/components/Header';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Header />
     </div>
   );
