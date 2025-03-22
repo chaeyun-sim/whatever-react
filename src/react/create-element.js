2d2a2d84eac9882206878ce8be20773bebedb6a3
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-22 13:47:58 +0900

    [Fix] createElement 수정

--- src/react/create-element.js
+++ src/react/create-element.js
@@ -1,11 +1,17 @@
 export function createElement(type, props, ...children) {
   const dom = {
     type,
-    props: {
-      ...props,
-      children: children.length === 1 ? children[0] : children,
-    },
+    props: {},
   };
 
-  return dom;
+  if (typeof type === 'string') {
+    dom.props = {
+      ...props,
+      children: children.length > 1 ? children : children[0],
+    };
+  } else if (typeof type === 'function') {
+    dom.props = type();
+  }
+
+  return Object.assign(dom);
 }
