665c2352ed46cad858e7256254360fafb54ac754
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-28 21:43:36 +0900

    [Feat] createRoot 적용 및 render 적용

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import App from '@/App';
+import { createRoot } from './react/create-root';
 
 const el = App();
 
 console.log(JSON.stringify(el, null, 2));
+
+const domNode = document.getElementById('app');
+const root = createRoot(domNode);
+
+root.render(App());
