d1add63fe821ba5b0ba3ebe305b49786549a6857
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-31 18:10:12 +0900

    [Fix] className prop 처리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ export default function App() {
         <h1>Title</h1>
       </header>
       <main>
-        <article>
-          <h2>Subtitle</h2>
+        <article aria-label='article'>
+          <h2 style={{ color: 'blue' }}>Subtitle</h2>
           <p>Content</p>
+          <input value={'1'} />
+          <button onClick={() => console.log('click')}>click!</button>
         </article>
       </main>
     </div>
--- src/react/render.js
+++ src/react/render.js
@@ -1,3 +1,28 @@
+function getProperty(element, props, propKey) {
+  const dangerousProps = ['innerHTML', 'outerHTML', 'insertAdjacentHTML'];
+  if (dangerousProps.includes(propKey)) {
+    console.warn(`보안 상의 이유로 ${propKey} 사용을 지양하세요.`);
+    return;
+  }
+
+  if (propKey === 'children') {
+    return;
+  } else if (propKey === 'style') {
+    Object.assign(element.style, props[propKey]);
+  } else if (propKey.startsWith('on')) {
+    const type = propKey.slice(2).toLowerCase();
+    element.addEventListener(type, props[propKey]);
+  } else if (propKey === 'ref') {
+    if (typeof props[propKey] === 'function') {
+      props[propKey](element);
+    } else {
+      props[propKey].current = element;
+    }
+  } else {
+    element.setAttribute(propKey, props[propKey]);
+  }
+}
+
 export function render(dom, root) {
   if (!dom) return null;
 
@@ -7,15 +32,7 @@ export function render(dom, root) {
     type === 'Fragment' ? document.createDocumentFragment() : document.createElement(type);
 
   if (props && props.children) {
-    Object.keys(props).forEach(propKey => {
-      if (propKey === 'children') {
-        return;
-      } else if (propKey === 'style') {
-        Object.assign(element.style, props[propKey]);
-      } else {
-        element.setAttribute(propKey, props[propKey]);
-      }
-    });
+    Object.keys(props).forEach(propKey => getProperty(element, props, propKey));
 
     props.children.forEach(child => {
       if (typeof child !== 'object' && child !== undefined) {
